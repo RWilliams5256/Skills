ff4474a7f6589b97273cd2b6a930d6d1c4fabf33
Ric Williams <rwilliams@experientgroup.com>
2018-05-31 11:24:31 -0400

    initial header image

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import logo2 from './images/logo2.png';
 import './App.css';
 
 class App extends Component {
@@ -7,13 +8,16 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          {/* <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1> */}
+           <img src={logo2} className="App-logo2" alt="logo2" />
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </p>        
+       
       </div>
+  
     );
   }
 }
