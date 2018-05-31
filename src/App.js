1e4d08385b8ecbb94292e32ebd5601c4b36a12bb
Ric Williams <rwilliams@experientgroup.com>
2018-05-31 16:18:35 -0400

    getting navigation underway - not complete yet

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import logo from './logo.svg';
 import logo2 from './images/logo2.png';
 import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Navigation from'./Components/navigation';
+
 
 class App extends Component {
   render() {
-    return (
+    return (      
       <div className="App">
         <header className="App-header">
-          {/* <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1> */}
-           <img src={logo2} className="App-logo2" alt="logo2" />
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>        
        
+           <img src={logo2} className="App-logo2" alt="logo2" />
+           <Navigation />
+        </header>       
       </div>
   
     );
