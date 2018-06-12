26d1a36de836cb8f1c77edaef6f23c97f64bee93
Ric Williams <rwilliams@experientgroup.com>
2018-06-12 09:48:39 -0400

    functionality and styling for the upload page

--- src/components/subcomponents/Upload.js
+++ src/components/subcomponents/Upload.js
@@ -2,30 +2,49 @@ import React, { Component } from 'react';
 import './Upload.css';
 
 class Upload extends Component{
+     
     constructor(props) {
         super(props);   
+      
+        this.handleFiles = this.handleFiles.bind(this);
 
-        this.handleChange = this.handleChange.bind(this);
-    }
-    
-    handleChange(event){
-        console.log("Selected file:", event.target.files[0]);
-    }
+        }
+
+        handleFiles() {  
+        var list = document.getElementById("myFile");
+        var file = document.getElementById("myFile").files[0];
+        var preview = document.getElementById("preview");
+        var para = document.createElement("p");
+
+        preview.removeChild(preview.childNodes[0]);
+        if(list.value.length) {          
+            para.textContent = file.name;                   
+        } else {
+            para.textContent = 'Please select a file to upload';
+        }
+        preview.appendChild(para);   
+
+    }  
 
     render() {
         return(
             <form>  
                 <label id="#bb">Choose file to upload              
-                    <input type="file" id="file" size="60" accept=".csv"/>
+                    <input classname="file" type="file" id="myFile" size="60" accept=".csv" onChange={this.handleFiles} />
                </label>
                <p/>
+               <div id="preview" class="preview">
+                    <p>Please select a file to upload </p>
+               </div>
                 <div>
-                    <button>Submit</button>
+                    <button classname="uploadButton">Upload</button>
                 </div>
             </form>
 
         );
     }
+
+ 
 }
 
 export default Upload;
