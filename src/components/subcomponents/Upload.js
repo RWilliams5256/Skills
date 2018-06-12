f9557543bc2ba9caad24624de314fcccaf340339
Ric Williams <rwilliams@experientgroup.com>
2018-06-12 16:10:52 -0400

    adding in a centralized firebase configuration

--- src/components/subcomponents/Upload.js
+++ src/components/subcomponents/Upload.js
@@ -9,8 +9,8 @@ class Upload extends Component{
         super(props);   
       
         this.handleFiles = this.handleFiles.bind(this);
-        this.processUpload = this.processUpload.bind(this); 
-
+        this.processUpload = this.processUpload.bind(this);  
+        this.saveHandshake = this.saveHandshake.bind(this);      
         }
 
         handleFiles() {  
@@ -29,29 +29,57 @@ class Upload extends Component{
 
     }  
 
-    processUpload = data => {
+    processUpload() {
         var file = document.getElementById("myFile").files[0];
-        Papa.parse(file,  {
+        Papa.parse(file,  { 
+            // header: true,             
             complete: function(results) {
-                alert(results.data);
+             
+
+                var arrayLength = results.data.length;
+                for(var i =1; i < arrayLength; i++)
+                {                   
+                    // [1] = application date (get date only)
+                    alert(results.data[i][1]);
+                    // [4] = student first name
+                    alert(results.data[i][4]);
+                    // [5] = student last name
+                    // [6] = student email
+                    // [9] = student school
+                    // [11] = student graduation date
+                    // [12] = majors
+                    // [15] = applied to name
+                }
+                
+
             }
         });
+    }  
+    
+    saveHandshake(data) {
+        var arrayLength = data.length;
+        for(var i =1; i < arrayLength; i++)
+        {
+           alert(data[i][1]);
+        }
+       
     }
 
+ 
     render() {
         return(
             <form>
                 <div> 
                     <label id="#bb">Choose file to upload              
-                        <input classname="file" type="file" id="myFile" size="60" accept=".csv" onChange={this.handleFiles} />
+                        <input className="file" type="file" id="myFile" size="60" accept=".csv" onChange={this.handleFiles} />
                     </label>
                </div>
                <p/>
-               <div id="preview" class="preview">
+               <div id="preview" className="preview">
                     <p>Please select a file to upload </p>
                </div>
                 <div>
-                    <button classname="uploadButton" onClick={this.processUpload}>Upload</button>
+                    <button className="uploadButton" onClick={this.processUpload}>Upload</button>
                 </div>  
                 <div id="results">
 
