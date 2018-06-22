52057d4f139116264c082ab6c971fc3a30d81b3b
Ryan Rodwell <ryanrodwell@gmail.com>
2018-06-22 08:53:31 -0400

    moving file into subcomponents

--- src/Components/Pages/Recruiting.js
+++ src/Components/Pages/Recruiting.js
@@ -1,46 +1,17 @@
 
 
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-// import PersonInfoForm from '../subcomponents/PersonInfoForm'
-// import SkillsForm from '../subcomponents/SkillsForm'
-// import ResumeForm from '../subcomponents/ResumeForm'
 import showResults from "../subcomponents/showResults";
 import { Values } from "redux-form-website-template";
-import FormOrchestrator from './FormOrchestrator';
+import FormOrchestrator from '../subcomponents/FormOrchestrator';
 
 
 
 class Recruiting extends Component {
 
-// constructor(props){
-//   super(props);
-//   this.nextPage = this.nextPage.bind(this)
-//     this.previousPage = this.previousPage.bind(this)
-//     this.state = {
-//       page: 1
-//     }
-
-// }
-
-  // nextPage() {
-  //   this.setState({ page: this.state.page + 1 })
-  // }
-
-  // previousPage() {
-  //   this.setState({ page: this.state.page - 1 })
-  // }
-
   render() {
-    // const { onSubmit } = this.props
-    // const { page } = this.state
-    return (
 
-      // <div>
-      //  {page === 1 && <PersonInfoForm  onSubmit={this.nextPage} />}
-      //  {page === 2 && <SkillsForm  previousPage={this.previousPage} onSubmit={this.nextPage}  />}
-      //  {page === 3 && <ResumeForm  previousPage={this.previousPage} onSubmit={onSubmit}/>}
-      // </div>
+    return (
 
       <div style={{ padding: 15 }}>
         <h2>Wizard Example</h2>
@@ -50,9 +21,9 @@ class Recruiting extends Component {
     )
   };
 }
-Recruiting.propTypes = {
-  onSubmit: PropTypes.func.isRequired
-}
+// Recruiting.propTypes = {
+//   onSubmit: PropTypes.func.isRequired
+// }
 
 export default Recruiting;
 
--- src/components/subcomponents/FormOrchestrator.js
+++ src/components/subcomponents/FormOrchestrator.js
@@ -2,31 +2,31 @@
 
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import PersonInfoForm from '../subcomponents/PersonInfoForm'
-import SkillsForm from '../subcomponents/SkillsForm'
-import ResumeForm from '../subcomponents/ResumeForm'
+import PersonInfoForm from './PersonInfoForm'
+import SkillsForm from './SkillsForm'
+import ResumeForm from './ResumeForm'
 // import WizardForm from "./Recruiting";
 
 
 class FormOrchestrator extends Component {
 
-constructor(props){
-  super(props);
-  this.nextPage = this.nextPage.bind(this)
-    this.previousPage = this.previousPage.bind(this)
-    this.state = {
-      page: 1
-    }
+  constructor(props){
+    super(props);
+    this.nextPage = this.nextPage.bind(this)
+      this.previousPage = this.previousPage.bind(this)
+      this.state = {
+        page: 1
+      }
 
-}
+  }
 
-nextPage() {
-  this.setState({ page: this.state.page + 1 })
-}
+  nextPage() {
+    this.setState({ page: this.state.page + 1 })
+  }
 
-previousPage() {
-  this.setState({ page: this.state.page - 1 })
-}
+  previousPage() {
+    this.setState({ page: this.state.page - 1 })
+  }
 
   render() {
     const { onSubmit } = this.props
@@ -41,6 +41,7 @@ previousPage() {
     )
   };
 }
+
 FormOrchestrator.propTypes = {
   onSubmit: PropTypes.func.isRequired
 }
