0cfa8c50d980d7e04dc52a572bea35839f31e677
Ryan Rodwell <ryanrodwell@gmail.com>
2018-07-01 18:13:10 -0400

    WIP building list for checkboxes

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3,9 +3,10 @@ import { db } from '../firebase/firebase';
 const helpers = {
 
     dbCallforLists: function(callback){
-        var categories = [];
-        // var allRoles = [];
-        // var allLevels =[];
+        let categories = [];
+        let everything = [];
+        // let allRoles = [];
+        // let allLevels =[];
 
         db.collection('lists').onSnapshot(function(querySnapshot) {
             querySnapshot.forEach(function(doc) {
@@ -20,6 +21,19 @@ const helpers = {
                     }
                 }
             });
+
+            console.log('query', querySnapshot.length)
+            // for(var i=0; i< categories.length;i++){
+            //     var choices = [];
+            //     querySnapshot.forEach(function(doc) {
+            //         const data = doc.data();
+            //         if( data.listName === categories[i]){
+            //             choices.push(data.name);
+            //         }
+            //     });
+            // }
+
+            // console.log('Everything', everything)
             sessionStorage.setItem('categories', categories)
             callback(categories);
         });
