ce93841015d7c9cc88e965b6f7e8ab63fac753a2
Ryan Rodwell <ryanrodwell@gmail.com>
2018-07-10 01:10:32 -0400

    populating checkboxes from DB

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -3,39 +3,45 @@ import { db } from '../firebase/firebase';
 const helpers = {
 
     dbCallforLists: function(callback){
-        let categories = [];
-        let everything = [];
+        let categories = []
+        let allCategories = [];
         // let allRoles = [];
         // let allLevels =[];
 
         db.collection('lists').onSnapshot(function(querySnapshot) {
             querySnapshot.forEach(function(doc) {
                 const data = doc.data();
-                // console.log(doc.id,data)
+                // console.log('data',data)
+
                 if(data.listName !== undefined) {
 
                     const categoryName = data.listName.toLowerCase()
 
                     if(!categories.includes(categoryName)){
                         categories.push(categoryName)
+                        const cat = {
+                            listName: categoryName,
+                            listItem: []
+                        };
+                        allCategories.push(cat)
                     }
                 }
             });
 
-            console.log('query', querySnapshot.length)
-            // for(var i=0; i< categories.length;i++){
-            //     var choices = [];
-            //     querySnapshot.forEach(function(doc) {
-            //         const data = doc.data();
-            //         if( data.listName === categories[i]){
-            //             choices.push(data.name);
-            //         }
-            //     });
-            // }
+            querySnapshot.forEach(function(doc) {
+                const data = doc.data();
+                if(data.listName !== undefined) {
+                    const categoryName = data.listName.toLowerCase()
+                    for(var i=0;i<allCategories.length;i++){
+                        if(allCategories[i].listName===categoryName){
+                            allCategories[i].listItem.push(data.name)
+                        }
+                    }
+                }
+            });
 
-            // console.log('Everything', everything)
-            sessionStorage.setItem('categories', categories)
-            callback(categories);
+            sessionStorage.setItem('allCategories', JSON.stringify(allCategories))
+            callback(allCategories);
         });
     },
 
