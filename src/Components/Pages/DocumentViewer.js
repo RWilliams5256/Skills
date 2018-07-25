1f1323ff95cf2318e909a69d05ae3be2f75507b3
Zack Baker <zbaker@experientgroup.com>
2018-07-25 10:37:22 -0400

    Added comments to DocumentViewer Component

--- src/Components/Pages/DocumentViewer.js
+++ src/Components/Pages/DocumentViewer.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 import './DocumentViewer.css';
 import FileViewer from 'react-file-viewer';
+
+//Because we are using a local file, we have to import the path
+//TODO: pass path into this component as prop
 import file from './pdfresume.pdf';
 
+//the FileViewer component takes in a path and a 'type' string.
+//TODO: pass as prop to this component
 const type = 'pdf';
 
 class DocumentViewer extends Component{
