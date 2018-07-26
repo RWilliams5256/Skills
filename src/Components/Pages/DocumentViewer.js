4262d3cdb9288c5b27ec725e1e79be635dfe749a
Zack Baker <zbaker@experientgroup.com>
2018-07-26 16:16:02 -0400

    PDF viewer working now
    in filecomponent.js there is now logic for displaying and loading both DOCX and PDF files.

--- src/Components/Pages/DocumentViewer.js
+++ src/Components/Pages/DocumentViewer.js
@@ -5,11 +5,11 @@ import FileComponent from '../subcomponents/FileComponent';
 
 //Because we are using a local file, we have to import the path
 //TODO: pass path into this component as prop
-import file from './docxresume.docx';
+import file from "./pdfresume.pdf";
 
 //the FileComponent component takes in a path and a 'type' string.
 //TODO: separete 'file' string at period and use the extension as 'type' so we dont have to store that in DB
-const type = 'docx';
+const type = 'pdf';
 
 class DocumentViewer extends Component{
     render(){
--- src/components/subcomponents/FileComponent.js
+++ src/components/subcomponents/FileComponent.js
@@ -3,41 +3,80 @@
 
 import React, { Component } from 'react';
 import mammoth from 'mammoth';
+import { Document, Page } from 'react-pdf/dist/entry.webpack';
+
+
 
 //TODO: Styles for docx
 
 export default class extends Component {
-  componentDidMount() {
+  state = {
+    file: this.props.filePath,
+    numPages: null,
+  }
 
-if(this.props.fileType === 'docx'){
-    const jsonFile = new XMLHttpRequest();
-    jsonFile.open('GET', this.props.filePath, true);
-    jsonFile.send();
-    jsonFile.responseType = 'arraybuffer';
-    jsonFile.onreadystatechange = () => {
-      if (jsonFile.readyState === 4 && jsonFile.status === 200) {
-        mammoth.convertToHtml(
-          { arrayBuffer: jsonFile.response },
-          { includeDefaultStyleMap: true },
-        )
-        .then((result) => {
-          const docEl = document.createElement('div');
-          docEl.className = 'document-container';
-          docEl.innerHTML = result.value;
-          document.getElementById('document').innerHTML = docEl.outerHTML;
-        })
-        .catch((a) => {
-          console.log('alexei: something went wrong', a);
-        })
-        .done();
-      }
-    };
-   }
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  }
+
+  componentDidMount() {
+    //When the component starts
+    //check if our file type is docx.
+    if (this.props.fileType === 'docx') {
+      //if fileType is docx create new xmlhttprequest
+      //then get file as arraybuffer and pass it to mammoth for conversion.
+      const jsonFile = new XMLHttpRequest();
+      jsonFile.open('GET', this.props.filePath, true);
+      jsonFile.send();
+      jsonFile.responseType = 'arraybuffer';
+      jsonFile.onreadystatechange = () => {
+        if (jsonFile.readyState === 4 && jsonFile.status === 200) {
+          mammoth.convertToHtml(
+            { arrayBuffer: jsonFile.response },
+            { includeDefaultStyleMap: true },
+          )
+            .then((result) => {
+              const docEl = document.createElement('div');
+              docEl.className = 'document-container';
+              docEl.innerHTML = result.value;
+              document.getElementById('document').innerHTML = docEl.outerHTML;
+            })
+            .catch((a) => {
+              console.log('alexei: something went wrong', a);
+            })
+            .done();
+        }
+      };
+      //if the fileType is pdf, use pdfjs to display
+    } else if (this.props.fileType === 'pdf') {
+      
+    }
   }
 
   render() {
+    
     return (
       <div id="document">
+       <Document 
+          file={this.state.file}
+          height="300"
+          onLoadSuccess={this.onDocumentLoadSuccess}
+          
+          >
+          {
+                Array.from(
+                  new Array(this.state.numPages),
+                  (el, index) => (
+                    <Page
+                      key={`page_${index + 1}`}
+                      pageNumber={index + 1}
+                      scale="1.5"
+                    />
+                  ),
+                )
+              }
+              </Document>
       </div>);
   }
 }
