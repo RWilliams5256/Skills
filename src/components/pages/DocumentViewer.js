c77cad9a17c68267a0c6fed90fb225ed0c9019fa
zbaker94 <zbaker94@gmail.com>
2018-07-27 11:11:43 -0400

    Add doc viewer and sample docs

---
+++ src/components/pages/DocumentViewer.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './DocumentViewer.css';
+import docxComponent from '../subcomponents/docxComponent';
+import pdfComponent from '../subcomponents/pdfComponent';
+
+//Because we are using a local file, we have to import the path
+//TODO: pass path into this component fom db
+import file from "./pdfresume.pdf";
+
+class DocumentViewer extends Component {
+
+    getDisplayComponent() {
+        var filePathArray = file.split(".");
+        const type = filePathArray[filePathArray.length - 1];
+
+        switch (type) {
+            case 'pdf': {
+                return pdfComponent
+            }
+            case 'docx': {
+                return docxComponent;
+            }
+            default: {
+                return;
+            }
+        }
+    }
+
+    render() {
+        const FileComponent = this.getDisplayComponent();
+
+        return (
+            <FileComponent
+                filePath={file}
+            />
+        );
+
+    };
+}
+
+export default DocumentViewer;
