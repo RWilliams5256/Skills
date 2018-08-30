f7875f923cc34827e5cfa611e0cab7de0c7f0b78
Ryan Rodwell <ryan.rodwell@accesscfa.com>
2018-08-30 11:34:07 -0400

    adding cloud function internally for fasteer call to db

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -40,8 +40,7 @@ const helpers = {
         });
     },
 
-    dbCallforPeople: function(listOfCriteria, callback) {
-
+    dbFetchPeople: function(listOfCriteria, callback) {
         var getUserByMultipleSkill = "https://us-central1-shomsi-test.cloudfunctions.net/getUserByMultipleSkill";
         var proxyurl = "https://cors-anywhere.herokuapp.com/";
         var data = JSON.stringify(listOfCriteria)
@@ -122,6 +121,60 @@ const helpers = {
    // return admin.firestore().doc('users/'+user.uid).set(userObject);
 
  },
+
+ dbCallforPeople(listOfCriteria, callback) {
+
+        var peopleRef = db.collection('people');
+        var person = listOfCriteria;
+        var data;
+        var query = peopleRef;
+        //console.log('req',req.body);
+
+        //console.log('skill',skill);
+        if (person.hasOwnProperty('skills')) {
+            for (var i in person.skills) {
+                query = query.where('skills.' + person.skills[i], '==', true)
+                // console.log('i is',i)
+            }
+        }
+        if (person.hasOwnProperty('currentProject')) {
+            query = query.where('currentProject', '==', person.currentProject)
+        }
+
+        if (person.hasOwnProperty('status')) {
+            query = query.where('status', '==', person.status)
+        }
+
+        if (person.hasOwnProperty('experienceLevel')) {
+            query = query.where('experienceLevel', '==', person.experienceLevel)
+        }
+        if (person.hasOwnProperty('role')) {
+            query = query.where('role', '==', person.role)
+        }
+        if (person.hasOwnProperty('referralSource')) {
+            query = query.where('refferedBy', '==', person.referralSource)
+        }
+
+        //  query = query.orderBy('skill.'+ skill[0])
+        //  console.log('query is', query);
+        query.get().then(snapshot => {
+            if (snapshot.empty) {
+                console.log('No documents found');
+            } else {
+                data = snapshot.docs.map(documentSnapshot => {
+                    return documentSnapshot.data();
+                })
+            //  console.log('Doc is', data);
+            }
+            callback(data);
+        })
+        .catch(err => {
+            console.log('Oops! Something went wrong.');
+            throw new Error(err);
+        });
+
+    }   
+
 };
 
 export default helpers;
